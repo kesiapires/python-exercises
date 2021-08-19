6b50c196d7ba67ece8999575e47c0e851fd2a5d9
Kesia Pires <kesiapiressantos@gmail.com>
2021-08-19 23:55:23 +0100

    while - last exercise - Kesia's Bank

--- progkesia71.py
+++ progkesia71.py
@@ -1,11 +1,30 @@
 #banco
 #dar cedulas 50 20 10 1
 contvalorsacado = 0
+cedulas = [50,20,10,1]
+notas = [0,0,0,0]
 
 while True:
     print('*' *30)
     print("BANCO KESIA")#{:^20}
     print("***************************************")
     valorsacado = int(input("Que valor você quer sacar? R$"))
-    total = valorsacado
-    cel = 50
+    total = valorsacado 
+    index = 0
+    for i in cedulas:
+        if total >= i:
+            notas[index] = total // i 
+            total = total % i   
+        index += 1
+    print("""Aqui estão suas notas: 
+    {} notas de 50 reais
+    {} notas de 20 reais
+    {} notas de 10 reais
+    {} notas de 1 real""".format(notas[0], notas[1], notas[2], notas[3]))
+    desejacont = str(input("Deseja continuar a operação? s/n")).strip().lower()
+    if desejacont != "s":
+        break 
+
+
+
+   
