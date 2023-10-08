59e39ab59db50baac67f87f586edb711b18388ba
Kesia Pires <kesiapiressantos@gmail.com>
2023-10-08 17:14:49 +0100

    Final Project

--- exercicio115/lib/arquivo/__init__.py
+++ exercicio115/lib/arquivo/__init__.py
@@ -26,4 +26,23 @@ def lerArquivo(nome):
         print(f'Não foi possível ler o arquivo {nome}')
     else:
         cabecalho('PESSOAS CADASTRADAS')
-        print(a.readlines())
+        for linha in a:
+            dado = linha.split(';')
+            dado[1] = dado[1].replace('\n', '')
+            print(f'{dado[0]:<30} {dado[1]:>3} anos')
+    finally:
+        a.close()
+
+def cadastrar(arq, nome='desconhecido', idade=0):
+    try:
+        a = open(arq, 'at')
+    except:
+        print('Houve um erro na abertura do arquivo.')
+    else:
+        try:
+            a.write(f'{nome}; {idade}\n')
+        except:
+            print('Houve um erro na hora de escrever os dados.')
+        else:
+            print(f'Novo registro de {nome} adicionado.')
+            a.close()
--- progkesia115.py
+++ progkesia115.py
@@ -17,11 +17,17 @@ while True:
     resposta = menu(['ver pessoas cadastradas', 'Cadastrar novas pessoas','Sair do sistema'])
     print(linha())
     if resposta == 1:
+        #opção de listar pessoas cadastradas
         print(f'Opção 1')
         lerArquivo(arq)
     elif resposta == 2:
+        #opção de cadastrar pessoas
+        nome = str(input('Digite seu nome: '))
+        idade = int(input('Digite sua idade: '))
+        cadastrar(arq,nome,idade)
         cabecalho('Opção 2')
     elif resposta == 3:
+        #opção de sair do sistema
         print(f'\033[4;36m Saindo do sistema... Até logo!\033[m')
         break
     else:
